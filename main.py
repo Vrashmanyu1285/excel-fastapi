78df12a464d04c2803357a38cc3b0f008f586195
Vrashmanyu1285 <vrash@pyrosens.com>
2025-08-13 12:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 app = FastAPI()
 
-EXCEL_PATH = "https://tempsensadmin-my.sharepoint.com/:x:/g/personal/vrash_pyrosens_com/EZnC56a7vIFHhjNsuPO_BgQBZ6xwvMgqysXQ0y3qRUcCGA?download=1"  # This file will be updated daily
+EXCEL_PATH = "data.xlsx"  # This file will be updated daily
 
 @app.get("/data")
 def get_data():
     df = pd.read_excel(EXCEL_PATH)
     return df.to_dict(orient="records")
+
 
